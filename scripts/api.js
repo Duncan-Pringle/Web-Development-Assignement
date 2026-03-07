1565bf64c1e72cb156269d6c1ff50b857526b975
Deen Mohameed Ratul Sarker <ratul109365@gmail.com>
2026-03-07 04:47:11 +0000

    api.js

---
+++ scripts/api.js
@@ -0,0 +1,40 @@
+// Manages all backend API requests 
+async function request(url, options={}) {
+    const response = await fetch (url,options);
+
+    if (! response.ok){
+       throw new Error("API reques failed");
+    }
+    return response.json();
+}
+
+export const API= {
+    // Popular movies
+    popular: () => request ("/api/popular"),
+
+    //Search movies
+    search: (query) => request ('/api/search?q=$(query)'),
+
+    // Login request 
+     login:(data) =>
+      request ("/api/login",{
+      method : "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify(data)
+    }),
+
+    //Get movie details 
+    movie:(id)=>request('/api/reviews/${id}'),
+
+    //Movie reviews
+    reviews:(id)=>request('/api/reviews/${id}'),
+
+    //Add review
+    addReview:(data)=>
+        request ("/api/review",{
+          method:"POST",
+          headers:{"Content-Type":"application/json"},
+          body:JSON.stringify(data)
+        })
+
+};

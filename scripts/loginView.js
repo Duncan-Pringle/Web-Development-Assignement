e862dcc808d00406d9fc90560e7d7b2f9f172b8a
Deen Mohameed Ratul Sarker <ratul109365@gmail.com>
2026-03-08 00:54:49 +0000

    loginView.js

---
+++ scripts/loginView.js
@@ -0,0 +1,12 @@
+// Login page user interface
+
+export function renderLogin(){
+    return `
+    <div class="login-box">
+    <h2> Login </h2>
+    <input id="username" placeholder="Username">
+    <input id="password" placeholder="Password">
+    <button id="submitLogin">Login</button>
+    </div>
+    `;
+}

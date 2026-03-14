e64ab6adb15b14bfb22f0710206692d85e8006b8
Duncan Pringle <duncanpringle@hotmail.co.uk>
2026-03-14 17:33:02 +0000

    Few updates which will hopefully fix the last bugs, forgot brackets on db.commit()

--- db_functions.py
+++ db_functions.py
@@ -6,7 +6,7 @@ import db as database
 def createUser(username, email, hashedPass):
     db = database.get_db()
     cursor = db.execute("INSERT INTO userTable (username, email, hashedPass) values (?, ?, ?)",(username,email, hashedPass,))
-    db.commit
+    db.commit()
     return cursor.lastrowid #Returns lastrowid so that you can create a user, and then immediately use their information without another select
 
 #Gets
@@ -44,28 +44,28 @@ def getIDFromUsername(username):
 def setUsername(userID, username):
     db = database.get_db()
     db.execute("UPDATE userTable SET username = ? where userID = ?", (username, userID))
-    db.commit
+    db.commit()
 
 def setEmail(userID, email):
     db = database.get_db()
     db.execute("UPDATE userTable SET email = ? where userID = ?", (email, userID))
-    db.commit
+    db.commit()
 
 def setLevel(userID, userLevel): #Used to update userlevel (default 1 = regular user)
     db = database.get_db()
     db.execute("UPDATE userTable SET userLevel = ? where userID = ?", (userLevel, userID))
-    db.commit
+    db.commit()
 
 def setPassword(userID, hashedPass):
     db = database.get_db()
     db.execute("UPDATE userTable SET hashedPass = ? where userID = ?", (hashedPass, userID))
-    db.commit
+    db.commit()
 
 #Removes a user from the database
 def deleteUser(userID):
     db = database.get_db()
     db.execute("DELETE FROM userTable where userID = ?", (userID))
-
+    db.commit()
 #==========Movie Functions==========
 
 #Inserts a movie into the movie table, and uses the genres json to insert the genres
@@ -74,7 +74,6 @@ def createMovie(movieID, title, description, poster_url, year, genres, rating):
     cursor = db.execute("INSERT INTO movie (movieID, title, description, poster_url, year, rating) values (?, ?, ?, ?, ?, ?)",
     (movieID, title, description, poster_url, year, rating)) #Inserts everything except for the genres                
     
-    movieID = cursor.lastrowid
 # Genre Format example from TMDB ----- [{"id": 18,"name": "Drama"}, {"id": 53,"name": "Thriller"}]
     for genre in genres:
         genreID = genre["id"]
@@ -83,12 +82,12 @@ def createMovie(movieID, title, description, poster_url, year, genres, rating):
         db.execute("INSERT OR IGNORE INTO genre (genreID, name) values (?, ?)", (genreID, genreName))
         #Insert into movieGenres join table
         db.execute("INSERT OR IGNORE INTO movieGenres (movieID, genreID) values (?, ?)", (movieID, genreID))
-    db.commit
+    db.commit()
 
 def createLikedMovie(userID, movieID):
     db = database.get_db()
     cursor = db.execute("INSERT INTO likedMovies (userID, movieID) values (?, ?)", (userID, movieID))
-    db.commit
+    db.commit()
     return cursor.lastrowid
                       
 #Gets
@@ -108,19 +107,19 @@ def getAllMovies(): #Returns json of all movies in the database
 def createReview(reviewText, userID, movieID):
     db = database.get_db()
     cursor = db.execute("INSERT INTO review (reviewText, userID, movieID) values (?, ?, ?)",(reviewText,userID, movieID,))
-    db.commit
+    db.commit()
     return cursor.lastrowid
 
 def deleteReview(reviewID):
     db = database.get_db()
     db.execute("DELETE FROM review WHERE reviewID = ?",(reviewID))
-    db.commit
+    db.commit()
     
 #Sets
 def setReviewText(reviewID, text):
     db = database.get_db()
     cursor = db.execute("UPDATE review SET reviewText = ? where reviewID = ?", (text, reviewID))
-    db.commit
+    db.commit()
     return cursor.lastrowid
 
     

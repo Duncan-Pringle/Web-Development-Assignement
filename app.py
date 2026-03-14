9a44664d39d8aa07122d8be55d88029a0aceebaf
Duncan Pringle <duncanpringle@hotmail.co.uk>
2026-03-14 18:28:09 +0000

    Removing tests, and adding database.db to gitignore, database.db is now stored directly on render

--- app.py
+++ app.py
@@ -9,18 +9,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route("/test") #Route for test route "https://web-development-assignement.onrender.com/test"
-def test_display():
-    try: 
-        test=db_functions.getAllUsers()
-        test.append(db_functions.getUserFromID(1))
-        test.append(db_functions.getUserFromEmail("username3@email.com"))
-        test.append(db_functions.getEmailFromID(2))
-        test.append(db_functions.getIDFromUsername("username1"))
-        return jsonify(test)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 @app.teardown_appcontext
 def close_connection(exception):
     database.close_connection(exception)

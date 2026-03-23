f28fc1c2386555a172286a30ff38289a6422013c
SpellstutterSprite <118872999+SpellstutterSprite@users.noreply.github.com>
2026-03-23 07:32:55 +0000

    add api/db integrated get_movie method

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import jsonify
 import db_functions
 import db as database
 import os
+import api
 
 app = Flask(__name__)
 
@@ -38,4 +39,38 @@ def testselects():
 def close_connection(exception):
     database.close_connection(exception)
 
-
+#Returns movie data from tmdb, but checks if we have it in the db first to instead use that
+#caches the film in the DB for next time if we don't have it and stores the poster url as a suffix, use api.TMDB_poster_url() to build the full URL whenever we need it
+@app.route('/movie/<int:movie_id>') 
+def get_movie(movie_id):
+    try:
+        movie = db_functions.getMovieFromID(movie_id)
+        if movie:
+            return jsonify(dict(movie)), 200
+ 
+        tmdb_data = api.TMDB_by_id(movie_id)
+        if "error" in tmdb_data:
+            return jsonify(tmdb_data), 502
+ 
+        db_functions.createMovie(
+            movieID=tmdb_data["id"],
+            title=tmdb_data["title"],
+            description=tmdb_data.get("overview"),
+            poster_url=tmdb_data.get("poster_path"),
+            year=tmdb_data.get("release_date", "")[:4] or None,
+            genres=tmdb_data.get("genres", []), 
+            rating=tmdb_data.get("vote_average")
+        )
+ 
+        return jsonify({
+            "movieID": tmdb_data["id"],
+            "title": tmdb_data["title"],
+            "description": tmdb_data.get("overview"),
+            "poster_url": tmdb.get_poster_url(tmdb_data.get("poster_path")),
+            "year": tmdb_data.get("release_date", "")[:4] or None,
+            "rating": tmdb_data.get("vote_average"),
+            "genres": tmdb_data.get("genres", [])
+        }), 200
+ 
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
